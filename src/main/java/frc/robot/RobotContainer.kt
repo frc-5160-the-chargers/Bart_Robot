caa16f9a2b18b8314f2d3257bf0629e320dc69db
DanielRocksUrMom <108989218+DanielRocksUrMom@users.noreply.github.com>
2023-08-23 09:43:28 -0400

    Initial Commit

---
+++ src/main/java/frc/robot/RobotContainer.kt
@@ -0,0 +1,56 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+package frc.robot
+
+import edu.wpi.first.wpilibj2.command.Command
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController
+import edu.wpi.first.wpilibj2.command.button.Trigger
+import frc.robot.Constants.OperatorConstants
+import frc.robot.commands.Autos
+import frc.robot.commands.ExampleCommand
+import frc.robot.subsystems.ExampleSubsystem
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the [Robot]
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and trigger mappings) should be declared here.
+ */
+class RobotContainer {
+    // The robot's subsystems and commands are defined here...
+    private val m_exampleSubsystem = ExampleSubsystem()
+
+    // Replace with CommandPS4Controller or CommandJoystick if needed
+    private val m_driverController = CommandXboxController(OperatorConstants.kDriverControllerPort)
+
+    /** The container for the robot. Contains subsystems, OI devices, and commands.  */
+    init {
+        // Configure the trigger bindings
+        configureBindings()
+    }
+
+    /**
+     * Use this method to define your trigger->command mappings. Triggers can be created via the
+     * [Trigger.Trigger] constructor with an arbitrary
+     * predicate, or via the named factories in [ ]'s subclasses for [ ]/[ PS4][edu.wpi.first.wpilibj2.command.button.CommandPS4Controller] controllers or [Flight][edu.wpi.first.wpilibj2.command.button.CommandJoystick].
+     */
+    private fun configureBindings() {
+        // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
+        Trigger { m_exampleSubsystem.exampleCondition() }
+            .onTrue(ExampleCommand(m_exampleSubsystem))
+
+        // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
+        // cancelling on release.
+        m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand())
+    }
+
+    val autonomousCommand: Command
+        /**
+         * Use this to pass the autonomous command to the main [Robot] class.
+         *
+         * @return the command to run in autonomous
+         */
+        get() =// An example command will be run in autonomous
+            Autos.exampleAuto(m_exampleSubsystem)
+}
